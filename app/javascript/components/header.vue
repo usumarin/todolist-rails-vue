d3321d204a0bde95ea65fbbff5a290d7384ef16b
usumarin <ubuntu2682ch@gmail.com>
2020-10-10 20:42:45 +0900

    add ヘッダーを作成

---
+++ app/javascript/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <v-toolbar color=deep-orange>
+      <v-toolbar-title>todolist</v-toolbar-title>
+    </v-toolbar>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "hello"
+}
+</script>
+
+<style>
+</style>
